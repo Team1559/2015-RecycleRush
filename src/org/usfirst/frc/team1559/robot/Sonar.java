39c87f82f85e2b59ea9f4a7171276852ccc83990
WillMerges <wdmerges@gmail.com>
2015-01-31 11:15:43 -0500

    Put classes in
    Some sonar stuff
    Murdered Robot.java to death

---
+++ src/org/usfirst/frc/team1559/robot/Sonar.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team1559.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class Sonar {
+
+	AnalogInput sensor;
+	double conversionFactor;
+	double dist;
+
+	public Sonar(int channel) {
+		sensor = new AnalogInput(channel);
+		conversionFactor = 9.2;
+	}
+
+	public double getInches() {
+		return ((sensor.getVoltage() * conversionFactor * 12));
+	}
+
+	public double getFeet() {
+		return ((sensor.getVoltage() * conversionFactor));
+	}
+
+	public double getVoltage() {
+		return (sensor.getVoltage());
+	}
+}
---
+++ src/org/usfirst/frc/team1559/robot/SonarStereo.java
@@ -0,0 +1,71 @@
+package org.usfirst.frc.team1559.robot;
+
+import java.text.DecimalFormat;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+
+public class SonarStereo {
+	private Sonar left;
+	private Sonar right;
+	private DigitalOutput pulse;
+	private int counter;
+
+	private DecimalFormat df;
+
+	/**
+	 * 
+	 * @param pulseChannel
+	 *            Digital output to trigger sonar chain
+	 * @param sonarLeft
+	 *            the analog channel of the left sonar
+	 * @param sonarRight
+	 *            the analog channel of the right sonar
+	 */
+	public SonarStereo(int pulseChannel, int sonarLeft, int sonarRight) {
+		df = new DecimalFormat("#.00");
+		pulse = new DigitalOutput(pulseChannel);
+		left = new Sonar(sonarLeft);
+		right = new Sonar(sonarRight);
+		counter = 0;
+	}
+
+	/**
+	 * Called periodically (assumes at least 20mS)
+	 */
+	public void periodic() {
+		final int START_PULSE = 0;
+		final int END_PULSE = START_PULSE + 1;
+		final int LEFT_SAMPLE = END_PULSE + 5;
+		final int RIGHT_SAMPLE = LEFT_SAMPLE + 5;
+
+		double lefty;
+		double righty;
+
+		switch (counter++) {
+
+		case START_PULSE:
+			pulse.set(true);
+			break;
+
+		case END_PULSE:
+			pulse.set(false);
+			break;
+
+		case LEFT_SAMPLE:
+			lefty = left.getFeet();
+			System.out.println("l: " + lefty);
+			break;
+
+		case RIGHT_SAMPLE:
+			righty = right.getFeet();
+			System.out.println("r: " + righty);
+			counter = 0;
+			break;
+		}
+	}
+
+	public void stop() {
+		counter = 0;
+		pulse.set(false);
+	}
+}
