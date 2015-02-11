6f8521346dd54d2b0fbf77ae9b738d93948991b8
chris hyser <chris.hyser@oracle.com>
2015-02-10 19:20:58 -0500

    library files

---
+++ src/org/usfirst/frc/team1559/robot/XBoxController.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team1559.robot;
+
+public class XBoxController {
+	public static final int BUTTON_A       = 1;
+	public static final int BUTTON_B       = 2;
+	public static final int BUTTON_X       = 3;
+	public static final int BUTTON_Y       = 4;
+	public static final int BUTTON_LB      = 5;
+	public static final int BUTTON_RB      = 6;
+	public static final int BUTTON_BACK    = 7;
+	public static final int BUTTON_START   = 8;
+	public static final int BUTTON_LSTICK  = 9;
+	public static final int BUTTON_RTSICK  = 10;
+	public static final int AXIS_LEFT_X    = 0;
+	public static final int AXIS_LEFT_Y    = 1;
+	public static final int AXIS_TRIGGER_L = 2;
+	public static final int AXIS_TRIGGER_R = 3;
+	public static final int AXIS_RIGHT_X   = 4;
+	public static final int AXIS_RIGHT_Y   = 5;
+}
