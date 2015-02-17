fb222077eebe022a7a1dfc4670564ba21ecbb841
WillMerges <wdmerges@gmail.com>
2015-02-17 09:16:20 -0500

    Forgot to delete comments......
    Forgot to uncomment some code in robot.java

--- src/org/usfirst/frc/team1559/robot/Robot.java
+++ src/org/usfirst/frc/team1559/robot/Robot.java
@@ -8,21 +8,21 @@ import edu.wpi.first.wpilibj.Talon;
 public class Robot extends IterativeRobot {
 	
 	SonarStereo ssonar = new SonarStereo(1, 0, 1);
-//	Talon leftFront, rightFront, leftBack, rightBack;
-//	SonarMovement move;
-//	Encoder pedometerX;
-//	Encoder pedometerY;
-//  int decision;
+	Talon leftFront, rightFront, leftBack, rightBack;
+	SonarMovement move;
+	Encoder pedometerX;
+	Encoder pedometerY;
+  int decision;
 	
 	public void robotInit() {
 		
-//		leftFront = new Talon(9);
-//		rightFront = new Talon(8);
-//		leftBack = new Talon(6);
-//		rightBack = new Talon(7);
-//		pedometerX = new Encoder(0, 1);
-//		pedometerY = new Encoder(2, 3);
-//		move = new SonarMovement(leftFront, rightFront, leftBack, rightBack, ssonar, pedometerX, pedometerY);
+		leftFront = new Talon(9);
+		rightFront = new Talon(8);
+		leftBack = new Talon(6);
+		rightBack = new Talon(7);
+		pedometerX = new Encoder(0, 1);
+		pedometerY = new Encoder(2, 3);
+		move = new SonarMovement(leftFront, rightFront, leftBack, rightBack, ssonar, pedometerX, pedometerY);
 	}
 
 	public void autonomousInit() {
@@ -30,7 +30,7 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void autonomousPeriodic() {
-		//move.avoid();
+		move.avoid();
     }
         
 
@@ -51,6 +51,6 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void disabledPeriodic() {
-        //move.disable();
+        move.disable();
 	}
 }
